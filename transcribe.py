8adca1ec4426ea93b6d876d20f3a48f504ba73f3
Kingston Whyte <kingstonwhyte7@gmail.com>
2026-01-05 12:16:08 +0000

    Changed transcribe to normal whisper because faster-whisper only looks for libcublas 12

--- transcribe.py
+++ transcribe.py
@@ -1,7 +1,9 @@
 import yaml
-from faster_whisper import WhisperModel
+#from faster_whisper import WhisperModel # faster-whisper isn't working at the moment, while i don't have cunting libcublas 12
+import whisper
 from settings import settings
 
+""" Using faster-whisper
 model = WhisperModel(**settings["transcribe"]["whisper"]) 
 def transcribe(audio_path):
     print(audio_path)
@@ -12,6 +14,13 @@ def transcribe(audio_path):
         content = content + f"{segment.text}"
 
     return content.strip()
+"""
+
+model = whisper.load_model(settings["transcribe"]["whisper"]["model_size_or_path"])
+def transcribe(audio_path):
+
+    result = model.transcribe(audio_path, fp16=False)
+    return result["text"]
 
 if __name__ == "__main__":
     print(transcribe("./input.wav"))
